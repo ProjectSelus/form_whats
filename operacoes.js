7319c11283ba7ec4bca0743cb9fa287099627834
Tiber <actiber@gmail.com>
2026-08-20 12:40:03 -0400

    Fix formatting of OPERACOES_VIGENTES string

--- operacoes.js
+++ operacoes.js
@@ -1,2 +1,2 @@
 // Edite apenas o texto entre as aspas abaixo
-const OPERACOES_VIGENTES = "01 (Operação Integrada Mulher Segura 2026";
+const OPERACOES_VIGENTES = "01 - Operação Integrada Mulher Segura 2026";
