8a0e98be1f0daf907208641254631f2fcc00b3fd
Tiber <actiber@gmail.com>
2026-08-28 14:31:12 -0400

    Update operacoes.js

--- operacoes.js
+++ operacoes.js
@@ -1,4 +1,2 @@
 // Edite apenas o texto entre as aspas abaixo
-const OPERACOES_VIGENTES = "02 
-  Op. Integrada Mulher Segura 2026 
-  Op. Força Total 30ª Edição";
+const OPERACOES_VIGENTES = "02: Op. Integrada Mulher Segura 2026 - Op. Força Total 30ª Edição";
