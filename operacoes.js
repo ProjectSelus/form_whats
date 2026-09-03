8f0d98c3c2bf2a5fe1d79359614fe534c5c53f49
Tiber <actiber@gmail.com>
2026-09-03 09:24:33 -0400

    Update OPERACOES_VIGENTES string value

--- operacoes.js
+++ operacoes.js
@@ -1,2 +1,2 @@
 // Edite apenas o texto entre as aspas abaixo
-const OPERACOES_VIGENTES = "01: Op. Integrada Mulher Segura 2026";
+const OPERACOES_VIGENTES = "02: Op. Integrada Mulher Segura 2026 - Op. PRODETOP 3B 2026";
